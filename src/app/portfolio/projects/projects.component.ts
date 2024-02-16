9d542cf23b22fa7855e0df508de6303773c06890
giucino <125276491+giucino@users.noreply.github.com>
2024-02-16 20:43:14 +0100

    Update app component, imprint component styles, package.json, portfolio component styles, preloader component styles, and styles.scss

--- src/app/portfolio/projects/projects.component.ts
+++ src/app/portfolio/projects/projects.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, QueryList, ViewChildren, ElementRef, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { isPlatformBrowser } from '@angular/common';
+
 
 @Component({
   selector: 'app-projects',
@@ -9,7 +11,40 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent {
-  constructor(public translate: TranslateService) { }
+export class ProjectsComponent implements AfterViewInit {
+  @ViewChildren('animateElement') elements!: QueryList<ElementRef>;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: object, public translate: TranslateService) { }
+
+
+  ngAfterViewInit() {
+    this.elements.forEach(element => {
+      this.observeElement(element.nativeElement);
+    });
+  }
+
+  private observeElement(element: Element) {
+    if (isPlatformBrowser(this.platformId)) {
+      if ('IntersectionObserver' in window) {
+        const observer = new IntersectionObserver((entries) => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              
+              entry.target.classList.add('animate__animated','animate__fadeInUp');
+              // observer.unobserve(entry.target);
+  
+              setTimeout(() => {
+                entry.target.classList.remove('animate__animated','animate__fadeInUp');
+              }, 4000); 
+            }
+          });
+        }, { threshold: [0.1] });
+  
+        observer.observe(element);
+      }
+    } else {
+      // Fallback-Logik für nicht-Browser-Umgebungen
+      element.classList.add('animate__animated','animate__fadeInUp');
+    }
+  }
 }
